40b00bdb74f5a8cc4bd96c42b0a0da0056a77123
Ashley Hoyt <turnerashley574@gmail.com>
2023-02-01 14:20:10 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Need to search for something?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Google
         </a>
       </header>
     </div>
